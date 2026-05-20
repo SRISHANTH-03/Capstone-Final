952f5327f2f5dcd13f6f0cc293742bb9809cbe1c
Srishanth <srishanthpagidimarry333@gmail.com>
2026-05-20 14:58:51 +0530

    changes

--- blogbackend-main/server.js
+++ blogbackend-main/server.js
@@ -11,7 +11,7 @@ config();
 const app = exp();
 
 app.use(cors({
-  origin: ["https://24eg107d46-capstone.netlify.app", "http://localhost:5173"],
+  origin: ["capstonefinall.netlify.app", "http://localhost:5173"],
   credentials: true,
 }));
 
